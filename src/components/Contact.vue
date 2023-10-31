6875d2d85d6191b0a358db5b02f137b1032eb094
Oza <oza.abhishek.hr@jp.fujitsu.com>
2023-10-31 22:01:40 +0900

    footer remaining

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,68 +1,41 @@
 <template>
-    <div class="contactContainer">
-        <div class="row">
-            <div class="col-md-6 col-sm-6 col-xs-12 bg-red full-height column flex-center q-py-md">
-                <h5 class="text-center">Ready To Start Your Voice Over Project with Me</h5>
-                <p style="font-size:large">Email : abhishekOza11@gmail.com</p>
-                <p style="font-size:large">Phone : 7841838287</p>
-                <q-btn label="Let's Connect"></q-btn>
+  <div class="contactContainer">
+    <div class="row">
+      <div class="col-md-6 col-sm-6 col-xs-12   column flex-center q-py-md">
+        <h5 class="text-center">Ready To Start Your Voice Over Project with Me</h5>
+        <p style="font-size:large">Email : abhishekOza11@gmail.com</p>
+        <p style="font-size:large">Phone : 7841838287</p>
+        <q-btn class="bg-white" label="Let's Connect"></q-btn>
+      </div>
+      <div class="col-md-6 col-sm-6 col-xs-12  q-pa-sm ">
+        <div class="bg-white shadow-2 full-height q-pa-md bordered">
+          <span class="q-pa-md" style="text-align: left; display: block; font-size: 30px; ">Get In Touch</span>
+          <form @submit="handleSubmit">
+            <div class="column q-pa-md ">
+              <label id="name">Name:</label>
+              <input for="name" type="text" class="form-control">
             </div>
-            <div class="col-md-6 col-sm-6 col-xs-12">
-                <q-card>
-              <q-card-section>
-                <div class="q-gutter-md">
-                  <div class="q-col-gutter-md">
-                    <q-form @submit="submitForm">
-                      <q-input
-                        v-model="formData.name"
-                        label="Name"
-                        outlined
-                      ></q-input>
-                      <q-input
-                        v-model="formData.email"
-                        label="Email"
-                        outlined
-                      ></q-input>
-                      <q-select
-                        v-model="formData.gender"
-                        label="Gender"
-                        outlined
-                        :options="genderOptions"
-                      ></q-select><q-btn
-                        label="Submit"
-                        color="primary"
-                        type="submit"
-                      ></q-btn>
-                    </q-form>
-                  </div>
-                </div>
-              </q-card-section>
-            </q-card>
-                <!-- <form @submit="handleSubmit" >
-                    <div class="column q-pa-md">
-                        <label id="name">Name:</label>
-                        <input for="name" type="text" class="form-control">
-                    </div>
-                    <div class="column q-pa-md">
-                        <label id="name">Email:</label>
-                        <input for="name" type="text" class="form-control">
-                    </div>
-                    <div class="column q-pa-md">
-                        <label id="name">Message:</label>
-                        <textarea for="name" type="text" class="textControl"/>
-                    </div>
-                    <div class="q-pa-md">
-                        <q-btn>Submit</q-btn>
-                    </div>
-                </form> -->
+            <div class="column q-pa-md">
+              <label id="name">Email:</label>
+              <input for="name" type="text" class="form-control">
             </div>
+            <div class="column q-pa-md">
+              <label id="name">Message:</label>
+              <textarea for="name" type="text" class="textControl" />
+            </div>
+            <div class="q-pa-md">
+              <q-btn class="bg-red-7">Submit</q-btn>
+            </div>
+          </form>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
-    name: 'contact',
-    data() {
+  name: 'contact',
+  data() {
     return {
       openDrawer: false,
       formData: {
@@ -78,24 +51,27 @@ export default {
       // Handle form submission here
       console.log("Form submitted:", this.formData);
     },
-}
+  }
 }
 </script>
 <style scoped>
 .contactContainer {
-    width: 100%;
+  width: 100%;
+  height: 100%;
+  min-height: 500px;
+  background-color: red;
 }
+
 .form-control {
-    width: 100%;
-    height: 40px;
-    font-size: 20px;
+  width: 100%;
+  height: 40px;
+  font-size: 20px;
 }
-.textControl{
-    height: 100%;
-    min-height: 100px;
+
+.textControl {
+  height: 100%;
+  min-height: 100px;
 }
 
 /* Define your custom CSS styles here */
-
-
 </style>
