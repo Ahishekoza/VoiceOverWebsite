93d84911a4452f2d8c676a7f086166b4b6a02078
Oza <oza.abhishek.hr@jp.fujitsu.com>
2023-11-10 15:06:17 +0900

    nodemailer added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import nodemailer from 'nodemailer'
 const app = express();
 
 
@@ -8,10 +9,43 @@ app.use(morgan('dev'))
 app.use(cors())
 app.use(express.json())
 
-app.get('/api',(req,res)=>{
-    res.status(200).json({
-        message:'Success'
+app.get('/sendEmail',(req,res)=>{
+
+    const {email,name,message} = req.body;
+
+    // transporter ---
+    const transporter = nodemailer.createTransport({
+        service:'gmail',
+        auth:{
+            user:'abhishekoza11@gmail.com',
+            pass:'sppk otzj sbps jtvu'
+        },
+        secure: true
+
     })
+
+    // mailOptions ---
+    const mailOptions = {
+        from : 'abhishekoza11@gmail.com',
+        to : 'abhishekoza11@gmail.com',
+        subject : 'New Inquiry from Website',
+        text : `Name :  ${name} , Email : ${email}  and message : ${message !== '' ?  message : 'Message Not Sent'}`
+    }
+
+    try {
+        transporter.sendMail(mailOptions).then((response)=>{
+            res.status(200).json({
+                message : 'Thank you for contacting'
+            })
+        })
+    } catch (error) {
+        res.status(404).json({
+            message:`error occurred ${error}`
+        })
+    }
+
+    // sendEmail ---
+
 })
 
 
